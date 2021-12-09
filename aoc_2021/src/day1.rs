6a251e240e38a80b173616564dbe44c5264bdbba
db <diogo.bernardo.dev@gmail.com>
2021-12-09 23:25:40 +0000

    day one completed

---
+++ aoc_2021/src/day1.rs
@@ -0,0 +1,44 @@
+#[inline]
+pub fn input() -> Vec<u64> {
+    let data: Vec<u64> = include_str!("day1.input")
+        .trim()
+        .lines()
+        .map(|l| l.parse::<u64>().unwrap())
+        .collect();
+    data
+}
+
+//all values should be positive
+pub fn part_a() -> u64 {
+    let data = input();
+    let mut answer = 0;
+    for i in data.windows(2) {
+        if i[1] > i[0] {
+            answer += 1;
+        }
+    }
+    answer
+}
+
+pub fn part_b() -> u64 {
+    let data = input();
+    let mut answer = 0;
+    for i in data.windows(4) {
+        if i[3] > i[0] {
+            answer += 1;
+        }
+    }
+    answer
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn part_a() {
+        assert_eq!(super::part_a(), 1301);
+    }
+    #[test]
+    fn part_b() {
+        assert_eq!(super::part_b(), 1346);
+    }
+}
