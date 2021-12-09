f88d3166231151029e17004862ba170c89d953fb
db <diogo.bernardo.dev@gmail.com>
2021-12-09 22:55:52 +0000

    removed target

--- aoc_2021/target/debug/deps/aoc-e9d2ea223f7934dd.d
+++
@@ -1,6 +0,0 @@
-/home/db/dev/Challenges/aoc_2021/target/debug/deps/aoc-e9d2ea223f7934dd.rmeta: src/lib.rs src/input.txt
-
-/home/db/dev/Challenges/aoc_2021/target/debug/deps/aoc-e9d2ea223f7934dd.d: src/lib.rs src/input.txt
-
-src/lib.rs:
-src/input.txt:
--- aoc_2021/target/debug/libaoc.d
+++
@@ -1,1 +0,0 @@
-/home/db/dev/Challenges/aoc_2021/target/debug/libaoc.rlib: /home/db/dev/Challenges/aoc_2021/src/input.txt /home/db/dev/Challenges/aoc_2021/src/lib.rs
